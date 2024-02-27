72464b0270b45b5c3c3cf56204e167501b0288a3
SlIzZe <yaro466122@gmail.com>
2024-02-27 21:43:42 +0600

    Box Shadow generator

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const box = document.querySelector(".box")
+const offsetX = document.querySelector(".offsetX")
+const offsetY = document.querySelector(".offsetY")
+const blur = document.querySelector(".blur")
+const stretch = document.querySelector(".stretch")
+const color = document.querySelector(".color")
+const output = document.querySelector(".output")
+const inset = document.querySelector(".inset")
+
+
+const arr = [offsetX, offsetY, blur, stretch, color, inset]
+const boxSdadowGeneration = () => {
+  let insetValue = ""
+  if(inset.checked) {
+    insetValue = 'inset'
+  }
+  box.style.boxShadow = `${offsetX.value}px ${offsetY.value}px ${blur.value}px ${stretch.value}px ${color.value} ${insetValue}`
+  output.innerText = box.style.boxShadow
+}
+
+arr.forEach(el => {
+  el.addEventListener('input', boxSdadowGeneration)
+})
